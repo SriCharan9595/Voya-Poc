fbe6d3f9be3d4905f48d05184215664eedabb2d8
Sricharan Kurapati <sk@Sricharans-MacBook-Pro.local>
2022-12-04 19:09:50 +0530

    first commit

---
+++ Services/IArithmetic.cs
@@ -0,0 +1,10 @@
+﻿namespace APIFunctionApp.Services
+{
+    public interface IArithmetic
+    {
+        string getAdd(int num1, int num2);
+        string getSub(int num1, int num2);
+        string getProd(int num1, int num2);
+        string getDiv(int num1, int num2);
+    }
+}
